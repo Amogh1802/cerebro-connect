9e439e000414e83716c385821008bb42cb76f18d
Amogh1802 <ghareamogh18@gmail.com>
2026-03-20 14:16:40 +0530

    fix: add Vercel URL to CORS config

--- src/main/java/com/cerebro/demo/config/SecurityConfig.java
+++ src/main/java/com/cerebro/demo/config/SecurityConfig.java
@@ -34,7 +34,8 @@ public class SecurityConfig {
                     CorsConfiguration config = new CorsConfiguration();
                     config.setAllowedOrigins(List.of(
                             "http://localhost:3000",
-                            "http://192.168.31.17:3000"  // local network
+                            "http://192.168.31.17:3000"
+                            "https://cerebro-frontend.vercel.app"// local network
                     ));
                     config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                     config.setAllowedHeaders(List.of("*"));
