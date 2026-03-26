9d7a7505ae78a93db5d4b1577fe1b21c6208b1b4
Amogh1802 <ghareamogh18@gmail.com>
2026-03-27 03:26:14 +0530

    x

--- src/main/java/com/cerebro/demo/controller/EEGRealTimeController.java
+++ src/main/java/com/cerebro/demo/controller/EEGRealTimeController.java
@@ -2,10 +2,7 @@ package com.cerebro.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
