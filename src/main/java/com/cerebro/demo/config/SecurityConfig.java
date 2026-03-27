e0ee9545472559b48ed2e3542b64327f00bf05cd
Amogh1802 <ghareamogh18@gmail.com>
2026-03-27 15:47:59 +0530

    eeg session permission

--- src/main/java/com/cerebro/demo/config/SecurityConfig.java
+++ src/main/java/com/cerebro/demo/config/SecurityConfig.java
@@ -61,6 +61,7 @@ public class SecurityConfig {
                         .requestMatchers("/error").permitAll()
                         .requestMatchers("/api/patients/**").permitAll()// This line allows assign patient
                         .requestMatchers("/api/users/**").permitAll()
+                        .requestMatchers("/api/eeg/sessions/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .exceptionHandling(ex -> ex
