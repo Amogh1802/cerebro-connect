66bc7bdf5eaa64ccf66ebf7d6365e60f918028ab
Amogh1802 <ghareamogh18@gmail.com>
2026-06-15 12:30:39 +0530

    temporary change

--- src/main/java/com/cerebro/demo/config/SecurityConfig.java
+++ src/main/java/com/cerebro/demo/config/SecurityConfig.java
@@ -60,6 +60,7 @@ public class SecurityConfig {
                         .requestMatchers("/error").permitAll()
                         .requestMatchers("/api/patients/**").permitAll()
                         .requestMatchers("/api/users/**").permitAll()
+                        .requestMatchers("/api/eeg/current-mode").permitAll()
                         .anyRequest().authenticated()
                 )
                 .exceptionHandling(ex -> ex
